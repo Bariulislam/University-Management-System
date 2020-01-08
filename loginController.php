992a04241496e20666e9920e8dad4f0533dbabb6
Bariulislam <bariulislam0021@gmail.com>
2020-01-08 22:22:03 +0600

    Add files via upload

---
+++ loginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\facades\DB;
+use App\User;
+use Validator;
+
+class loginController extends Controller
+{
+    public function index(){
+		return view('login.index');
+	}
+	
+	public function verify(Request $req){
+		$validation = Validator::make($req->all(), [
+            'username'=>'required',
+            'password'=>'required',
+        ]);
+        if($validation->fails()){
+            return back()
+                    ->with('errors', $validation->errors())
+                    ->withInput();
+		}
+		
+		$user = User::where('username', $req->username)
+					->where('password', $req->password)
+					->first();
+					
+		if($user){
+	
+			$req->session()->put('name', $req->input('username'));
+			$req->session()->put('user', $user->job);
+			echo "login Done";
+			
+			return redirect()->route('home.index');
+			
+		}else{
+			$arr = array(
+				'Wrong Password!'
+			);
+			$req->session()->flash('msg', '*Invalid username/password');
+			return back();
+            
+		}
+	}
+}
